5d488e29cdc3fda111b1c76afc6a1193650b7a12
aburch13 <ab13189@gmail.com>
2019-11-16 00:14:50 -0500

    Character Prefab

---
+++ 232 Final/Assets/Scripts/Soul.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soul : MonoBehaviour
+{
+    public static int souls;
+    public ParticleSystem part;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(true||other.tag == "Player")
+        {
+            souls++;
+            part.Play();
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(0,0,0,0);
+        }
+    }
+}
---
+++ 232 Final/Assets/Scripts/TapSwipe.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapSwipe : MonoBehaviour
+{
+    public Rigidbody2D body;
+    public BoxCollider2D hit;
+    public BoxCollider2D foot;
+    private bool grounded, attacking;
+
+    private Touch touch;
+    private float time = 0;
+
+    private Vector2 beginTouchPosition, endTouchPosition;
+    void Update()
+    {
+        if (foot.isTrigger && body.velocity.y < 0 && foot.OverlapCollider(new ContactFilter2D(), new Collider2D[5]) == 0)
+        {
+            foot.isTrigger = false;
+        }
+        if(attacking)
+        {
+            time += Time.deltaTime;
+            if (time > .5)
+            {
+                attacking = false;
+                time = 0;
+            }
+        }
+        else if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    beginTouchPosition = touch.position;
+                    break;
+
+                //case TouchPhase.Moved:
+
+                case TouchPhase.Ended:
+                    endTouchPosition = touch.position;
+
+                    //tap
+                    if (beginTouchPosition == endTouchPosition)
+                    {
+                        hit.enabled = true;
+                        attacking = true;
+                        Debug.Log("tapping");
+                    }
+                    //swipe
+                    if (!attacking && grounded && beginTouchPosition != endTouchPosition)
+                    {
+                        foot.isTrigger = true;
+                        grounded = false;
+                        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 16);
+                    }
+                    if (!attacking && (endTouchPosition.y < beginTouchPosition.y) && transform.position.y > -3.5f)
+                    {
+                        grounded = false;
+                        foot.isTrigger = true;
+                        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -50);
+                    }
+                    Debug.Log("swiping");
+                    
+                    break;
+            }
+        }
+    }
+}
